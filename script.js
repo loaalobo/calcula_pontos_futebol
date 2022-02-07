feeae411cf0b020fd226a8814bb5daf41a16edb0
Lorena Lobo <74051141+loaalobo@users.noreply.github.com>
2022-02-07 18:36:48 -0300

    feat: adiciona funcionalidade de exibir a data e altera detalhes de estilo

--- script.js
+++ script.js
@@ -21,6 +21,29 @@ function calculaPontos() {
 }
 
 function exibeResultado() {
+    let dia = new Date().getDate();
+
+    const nomeTime = document.getElementById('nomeTime').value;
     const resultado = document.getElementById('resultado');
-    resultado.innerHTML = "Total: " + calculaPontos();
+    resultado.innerHTML = "Até o dia " + dia + " de " + retornaMes() + ", o " + nomeTime + " fez " + calculaPontos() + " pontos.";
 }
+
+function retornaMes() {
+    let mes = new Date().getMonth() + 1;
+
+    switch(mes) {
+        case 1: return 'Janeiro';
+        case 2: return 'Fevereiro';
+        case 3: return 'Março';
+        case 4: return 'Abril';
+        case 5: return 'Maio';
+        case 6: return 'Junho';
+        case 7: return 'Julho';
+        case 8: return 'Agosto';
+        case 9: return 'Setembro';
+        case 10: return 'Outubro';
+        case 11: return 'Novembro';
+        case 12: return 'Dezembro';
+    }
+}
+
